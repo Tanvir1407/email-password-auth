af7ebcfa74bc775fdad61ba1183cb755c6805dae
MD. Tanvir <mt14050@gmail.com>
2023-01-27 21:28:58 +0600

    create a login page

--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
 import "./App.css";
-import { getAuth } from "firebase/auth";
-import app from "./Firebase/firebase.init";
-import { useState } from "react";
+import Login from "./components/Login";
 
-const auth = getAuth(app);
 
 function App() {
-  const [check, setCheck] = useState(false);
-  const handleRegister = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log(email, password);
-    e.target.email.value = "";
-    e.target.password.value = "";
-  };
-
-  const checkboxHandle = (e) => {
-    setCheck(e.target.checked);
-  };
+  
   return (
-    <div className="mx-auto w-1/3">
-      <form onSubmit={handleRegister}>
-        <input
-          className="border-2 border-red-500 rounded p-1 m-1"
-          type="email"
-          placeholder="Enter Your Email"
-          name="email"
-          id=""
-        />{" "}
-        <br />
-        <input
-          className="border-2 border-red-500 rounded p-1 m-1"
-          type="password"
-          placeholder="Enter your Password"
-          name="password"
-          id=""
-        />
-        <br />
-        <label className="m-2" htmlFor="check">
-          <input onClick={checkboxHandle} type="checkbox" name="check" id="" />{" "}
-          I accept{" "}
-          <a href="#" className="text-blue-500">
-            Terms and Conditions
-          </a>{" "}
-          <br />
-        </label>
-        {check ?<button
-          className="bg-gray-400 px-14 hover:bg-slate-900 hover:text-white duration-500 rounded-md py-2 "
-          type="submit"
-        >
-          Registration
-        </button> : <button
-          className="bg-gray-400 px-14  duration-500 rounded-md py-2"
-            type="submit"
-            disabled
-        >
-          Registration
-        </button>}
-      </form>
-    </div>
+    <>
+      <Login> </Login>
+    </>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useState } from "react";
+import app from "../Firebase/firebase.init";
+
+const auth = getAuth(app);
+
+const Login = () => {
+
+    const [check, setCheck] = useState(false);
+
+    
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+      createUserWithEmailAndPassword(auth, email, password)
+          .then(result => {
+              console.log(result);
+          })
+          .catch(err => {
+              console.log(err);
+      })
+    e.target.email.value = "";
+    e.target.password.value = "";
+  };
+  
+  const checkboxHandle = (e) => {
+    setCheck(e.target.checked);
+  };
+  return (
+    <div className="mx-auto w-1/3">
+      <form onSubmit={handleRegister}>
+        <h1 className="text-3xl text-cyan-500 font-semibold my-4">
+          Please Register..!!!
+        </h1>
+        <input
+          className="my-3 border-2 min-w-full border-gray-500 rounded p-2 m-1"
+          type="email"
+          placeholder="Enter Your Email"
+          name="email"
+          id=""
+          required
+        />{" "}
+        <br />
+        <input
+          className="my-3  border-2 min-w-full border-gray-500 rounded p-2 m-1"
+          type="password"
+          placeholder="Enter your Password"
+          name="password"
+          id=""
+          required  
+        />
+        <br />
+        <label className="m-2" htmlFor="check">
+          <input onClick={checkboxHandle} type="checkbox" name="check" id="" />{" "}
+          I accept{" "}
+          <a href="#" className="text-blue-500">
+            Terms and Conditions
+          </a>{" "}
+          <br />
+        </label>
+        {check ? (
+          <button
+            className="my-3   bg-green-400 px-12 min-w-full font-bold hover:bg-green-600 text-white duration-500 rounded-md py-2 "
+            type="submit"
+          >
+            Registration
+          </button>
+        ) : (
+          <button
+            className=" my-3 text-gray-500  bg-gray-300 px-12 min-w-full font-bold duration-500 rounded-md py-2"
+            type="submit"
+            disabled
+          >
+            Registration
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
