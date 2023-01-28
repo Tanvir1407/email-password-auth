1c5684a5b7bcbc151723dbde54f853d85aef0373
MD. Tanvir <mt14050@gmail.com>
2023-01-28 15:25:32 +0600

    Password Stroger And check validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,20 +7,34 @@ const auth = getAuth(app);
 
 const Login = () => {
 
-    const [check, setCheck] = useState(false);
+  const [check, setCheck] = useState(false);
+  const [passwordError, setPasswordError] = useState(['']);
 
-    
   const handleRegister = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+      e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError('Please, add two uppercase letter');
+      return;
+    }
+    if (!/(?=.*[!@#$&*])/.test(password)) {
+      setPasswordError('Please, add one spacial chracters');
+      return;
+    }
+    if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+      setPasswordError('Please, add two digit');
+      return
+    }
+    setPasswordError('');
       createUserWithEmailAndPassword(auth, email, password)
           .then(result => {
               console.log(result);
           })
           .catch(err => {
               console.log(err);
-      })
+          })
+
     e.target.email.value = "";
     e.target.password.value = "";
   };
@@ -51,6 +65,7 @@ const Login = () => {
           id=""
           required  
         />
+        <p className="text-red-300">{passwordError }</p>
         <br />
         <label className="m-2" htmlFor="check">
           <input onClick={checkboxHandle} type="checkbox" name="check" id="" />{" "}
@@ -60,7 +75,7 @@ const Login = () => {
           </a>{" "}
           <br />
         </label>
-        {check ? (
+        {check  ? (
           <button
             className="my-3   bg-green-400 px-12 min-w-full font-bold hover:bg-green-600 text-white duration-500 rounded-md py-2 "
             type="submit"
